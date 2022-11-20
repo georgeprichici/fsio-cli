2243ee9314e825696ea64f7b2b018f76674db8fa
georgeprichici <4246227+georgeprichici@users.noreply.github.com>
2022-11-20 15:27:26 -0800

    Add mdcore as a new allowed command.

--- filescan.py
+++ filescan.py
@@ -5,6 +5,7 @@ from filescan_cli.commands.report import report
 from filescan_cli.commands.reports import reports
 from filescan_cli.commands.system import system
 from filescan_cli.commands.files import files
+from filescan_cli.commands.mdcore import mdcore
 
 
 @aclick.group(help="CLI tool to access to the filescan service")
@@ -12,6 +13,6 @@ def cli():
     pass
 
 
-cli_groups = aclick.CommandCollection(sources=[conf, scan, report, reports, system, files])
+cli_groups = aclick.CommandCollection(sources=[conf, scan, report, reports, system, files, mdcore])
 if __name__ == "__main__":
     cli_groups(_anyio_backend="asyncio")  # or asyncio, or curio
