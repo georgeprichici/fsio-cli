ab6815144d2cd41952051df9a5907a2cbf23e84a
georgeprichici <4246227+georgeprichici@users.noreply.github.com>
2022-11-28 10:38:24 -0800

    Add filename as a header, so it won't use temp file names.

--- filescan_cli/flow/mdcore_scan.py
+++ filescan_cli/flow/mdcore_scan.py
@@ -3,7 +3,7 @@ from filescan_cli.core.logger import Logger
 from filescan_cli.service.scan import Scan
 from filescan_cli.service.report import Report
 from filescan_cli.formatter.mdcore_reports import MDCoreReportsFormatter
-
+import json
 
 class MDCoreScanFlow:
     """Scanning flow"""
@@ -23,19 +23,20 @@ class MDCoreScanFlow:
         tags,
         prop_tags,
         password,
-        is_private
+        is_private, 
+        filename
     ):
         """Upload a file or link and receive its report"""
 
-        scan_id = await self.__upload(file, link, desc, tags, prop_tags,password, is_private)
+        scan_id = await self.__upload(file, link, desc, tags, prop_tags,password, is_private, filename)
         mdcore_response = await self.__get_scan_reports(scan_id)
 
         return mdcore_response
 
 
-    async def __upload(self, file, link, desc, tags, prop_tags, password, is_private):
+    async def __upload(self, file, link, desc, tags, prop_tags, password, is_private, filename):
 
-        result = await self.scanner.upload(file, link, desc, tags, prop_tags,password, is_private)
+        result = await self.scanner.upload(file, link, desc, tags, prop_tags,password, is_private, filename)
 
         if 'error' in result:            
             return
@@ -43,26 +44,31 @@ class MDCoreScanFlow:
         response = result['content']
         if 'flow_id' not in response:
             return
-
+        # self.logger.error(f"report id: {response['flow_id']}")
         return response['flow_id']
 
 
     async def __get_scan_reports(self, scan_id):
         """Get reports related to scan"""
 
-
         while True:
             await asyncio.sleep(1)
 
-            result = await self.report.get_scan_reports(scan_id, filters=['finalVerdict'])
+            result = await self.report.get_scan_reports(scan_id, filters=['finalVerdict', 'overallState'])
             if 'error' in result:
                 return
-
+            
             scan_report = result['content']
+            
             if 'reports' not in scan_report or not scan_report['reports']:
                 continue
-            
-            reports = scan_report['reports']     
+                        
+            reports = scan_report['reports']                              
                 
-            return self.formatter.format(reports)
+            mdcore_result =  self.formatter.format(reports)
+
+            if not mdcore_result:
+                continue
+            else:
+                return mdcore_result
 
