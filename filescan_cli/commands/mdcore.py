1f40ca6a5e33f7f9065fc6d9322ce9b996cb1b0d
georgeprichici <4246227+georgeprichici@users.noreply.github.com>
2022-11-28 10:37:31 -0800

    Check blockedOnly flag

--- filescan_cli/commands/mdcore.py
+++ filescan_cli/commands/mdcore.py
@@ -16,12 +16,10 @@ def mdcore():
 
 @mdcore.command('mdcore', short_help='Integrate with MetaDefender Core as External Scanner or Post Action')
 @aclick.option('--config', type=str, is_flag=False, default='', help='Path to the config file')
-@aclick.option('--workflow', type=str, is_flag=False, default='', help='Specify for which worflow to run the analysis')
 @aclick.option('--blocked', type=bool, is_flag=True, default=False, help='Process only files marked as Blocked')
 @aclick.argument('filename', type=str, required=True)
 async def submit(
     config,
-    workflow,
     blocked,
     filename
 ):
@@ -29,39 +27,30 @@ async def submit(
 
     logger = Logger()
 
-    
-
     # TODO: check existing report based on hash? 
     # md5 = scan_results["file_info"]["md5"]
+
+
+    data = sys.stdin.readlines()	    
+    scan_results = json.loads(data[0])
+
     result = {}
-    if (__will_run_analysis(workflow, blocked)):
+    if (__will_run_analysis(scan_results, blocked)):
 
         scan_flow = MDCoreScanFlow()
-        result = await scan_flow.run(file=filename, link=None, desc='', tags='', prop_tags=False, password='', is_private=False)     
+        original_name = scan_results['file_info']['display_name']
+        result = await scan_flow.run(file=filename, link=None, desc='', tags='', prop_tags=False, password='', is_private=False, filename=original_name)     
     else:
           
         result = MDCoreReportsFormatter().empty_result()
                 
-
     # logger.debug(json.dumps(result, indent=3))
     sys.stdout.write(json.dumps(result, indent=3))
+    # sys.stdout.write(str(result))
     return result
 
-def __will_run_analysis(workflow, blockedOnly):
-    
-    data = sys.stdin.readlines()	
-    scan_results = json.loads(data[0])
+def __will_run_analysis(scan_results, blockedOnly):
 
-    is_file_blocked = scan_results['process_info']['result'] == "Blocked"
-    original_name = scan_results['file_info']['display_name']	# TODO: pass the original name to filescan.io
-    current_rule = scan_results["process_info"]["profile"]
-
-    if workflow and workflow != current_rule: 
-        return False
-
-    if not blockedOnly or (blockedOnly and is_file_blocked):
-        return True
-
-    return False
-
+    is_file_blocked = scan_results['process_info']['result'] == "Blocked"    
+    return not blockedOnly or (blockedOnly and is_file_blocked)         
     
